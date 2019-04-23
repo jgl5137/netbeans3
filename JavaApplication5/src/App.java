6486edeec2386d9e159d25b971f501e83591531e
jgl5137 <jgl5137@psu.edu>
2019-04-23 14:39:10 -0400

    edited java file

--- JavaApplication5/src/App.java
+++ JavaApplication5/src/App.java
@@ -1,4 +1,4 @@
-/*
+*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,6 +15,7 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+	System.out.println("WOAH KENNY");
     }
     
 }
